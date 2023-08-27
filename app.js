40a914196ebc2afc685ae042bdb194ea6de7f68f
KavishPawar <kavishp165@gmail.com>
2023-08-27 14:17:39 +0530

    commit first

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ app.set('view engine', 'ejs');
 //show index
 app.get('/', (req,res) => {
     res.render('index', { title: "Home Page"});
+
+
+
+
+app.use( require("express").json() );
+
+app.post("/", async (req,res) => {
+    // console.log( req.body[1] ); // <== Receives: [ arr ]
+    // console.log( req.body[2] ); // <== Receives: [ arr ]
+    res.status(200).json({ received: req.body });
+
+    // const apiUrl = req.body[0];
+    // const lang = req.body[1];
+    // const star = req.body[2];
+
+    const finalUrl = req.body[0] + req.body[1] + req.body[2];
+    let ripped = await fetch(finalUrl)
+     let rip = await ripped.json();
+     console.log( rip );
+    //  res.end();
+     res.json(rip.items)
+
+
+    // res.send('index') // Send back a confirmation JSON response
+});
 } )
 
 
--- static/script.js
+++ static/script.js
@@ -1,33 +1,48 @@
-
 const searchBtn = document.querySelector('.search_button');
 const inputBox = document.querySelector('#lang');
 const inputStar = document.querySelector('#stars');
 
-const profilePic = document.querySelector('.profile_img');
-const DateUpdated = document.querySelector('.updatedDate');
-const forks = document.querySelector('.forks');
+// const profilePic = document.querySelector('.profile_img');
+// const DateUpdated = document.querySelector('.updatedDate');
+// const forks = document.querySelector('.forks');
+// const starCount = document.querySelector('.starCount');
 
 searchBtn.addEventListener('click', function(e) {
 
     const apiUrl = 'https://api.github.com/search/repositories';
     const lang = "?q=language:" + inputBox.value;
-    const star = "?q=stars:" + inputStar.value;
-    // const star = "?q=stars%3A" + inputStar.value;
+    const star = "+stars:" + inputStar.value;
+    // const star = "?q=stars%3A" + inputStar.value;\
 
+    let arr = [];
     alert( apiUrl + lang + star );
-    const finalUrl = fetch( apiUrl + lang + star)
-        .then((result) => result.json())
-        .then((data) => {
-            console.log( data );
+    arr.push(apiUrl, lang, star );
+    console.log(arr);
+    fetch('https://localhost:3000'), {
+        method: 'POST',
+        header: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(arr)
+    }
+
+
+    // const finalUrl = fetch( apiUrl + lang + star)
+    //     .then((result) => result.json())
+    //     .then((data) => {
+            // console.log( data );
+
             // data.items.forEach( (e) => {
             //     let pic = e.owner.avatar_url ;
             //     let upd = e.updated_at;
+            //     let star = e.stargazers_count ;
             //     let frk = e.forks_count;
-            //     console.log(pic, upd, frk );
+            //     console.log(pic, upd, frk, star);
             //     profilePic.src = pic;
             //     DateUpdated.innerHTML =` Date Updated :   ${upd}`;
             //     forks.innerHTML = `Forks :   ${frk}`;
+            //     starCount.innerHTML = `Stars : ${star}`;
             // });
-        })
-        .catch((err) => console.log("error occured", err));
+        // })
+        // .catch((err) => console.log("error occured", err));
 })
