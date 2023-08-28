7d061b041ba5705744742408d617e3bd282316da
KavishPawar <kavishp165@gmail.com>
2023-08-28 20:54:15 +0530

    first commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post("/", async (req,res) => {
     // res.status(200).json({ received: req.body });//gives response after getting a req. it stops the process and wont process the rest of the code.
     console.log(req.body);
 
-    const finalUrl = req.body[0] + req.body[1] + req.body[2];
+    const finalUrl = req.body[0];
     let ripped = await fetch(finalUrl)//fetches and stored in the variable ripped.
     let rip = await ripped.json();//converts the REABABLESTREAM from the fetched varible(ripped) into a javascript object.
     console.log( rip );
--- static/script.js
+++ static/script.js
@@ -2,17 +2,35 @@
 const searchBtn = document.querySelector('.search_button');
 const inputBox = document.querySelector('#lang');
 const inputStar = document.querySelector('#stars');
+const inputDate = document.querySelector('#dates');
 
 searchBtn.addEventListener('click', async (e) => {
 
     const apiUrl = 'https://api.github.com/search/repositories';
     const lang = "?q=language:" + inputBox.value;
-    const star = "+stars:" + inputStar.value;
+    const star = inputStar.value;
+    const date = inputDate.value;
 
+    let arr1 = `${apiUrl}`;
+    // alert( apiUrl + lang + star );
+    // arr.push(apiUrl, lang, star );
+
+    if(lang){
+        arr1 += lang;
+    }
+    if(star){
+        arr1 += `+stars:${star}` ;
+    }
+    if(date){
+        arr1 += `+dates:${date}` ;
+    }
+
+    console.log(arr1, typeof arr1)
     let arr = [];
-    alert( apiUrl + lang + star );
-    arr.push(apiUrl, lang, star );
-
+    arr.push(arr1);
+    console.log(arr, typeof arr)
+    let stri = JSON.stringify(arr);
+    console.log(stri, typeof stri)
 
 
 let sendData = await fetch("/", {
@@ -29,14 +47,19 @@ let sendData = await fetch("/", {
         let profile_pic = e.owner.avatar_url;
         let star_count = e.stargazers_count;
         let fork_count = e.forks_count;
-        let profile_name = e.name;
+        let profile_name = e.full_name;
+        let description = e.description;
         let updated_date = e.updated_at;
-         // console.log(`user${i}=> ${profile_pic}, ${star_count}, ${fork_count}`)
-        renderCards(profile_pic, profile_name, updated_date, star_count, fork_count);
+        let language = e.language;
+
+        renderCards(profile_pic,profile_name, updated_date, star_count, fork_count, language, description);
     });
 })
 
-function renderCards(profile_pic, profile_name, updated_date, star_count, fork_count) {
+
+
+
+function renderCards(profile_pic, profile_name, updated_date, star_count, fork_count, language, description) {
     let cardList = '';
     cardList += `
         <div class="main_card">
@@ -45,11 +68,15 @@ function renderCards(profile_pic, profile_name, updated_date, star_count, fork_c
                 <div class="profile_name">${profile_name}</div>
             </div>
             <div class="middle_section">
-                <span>Stars : ${star_count}</span>
-                <span>Fork : ${fork_count}</span>
+                <div class="description">${description}</div>
+                
             </div>
             <div class="bottom_section">
-                <div class="updated-date">Updated on: ${updated_date}</div>
+                <div class="ball"></div>
+                <div class="language">${language} </div>
+                <div class="stars"> Stars: ${star_count} </div>
+                <div class="updated-date">Updated on: ${updated_date} </div>
+                <div> Fork: ${fork_count} </div>
             </div>
         </div>
         `
